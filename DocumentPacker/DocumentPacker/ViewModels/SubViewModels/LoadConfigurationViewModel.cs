babf187cf431a8339605d7cd4668d0c7fdab2d45
MichaelDiers <md@sreid.de>
2024-12-01 23:09:53 +0100

    add load configuration view model properties

--- DocumentPacker/DocumentPacker/ViewModels/SubViewModels/LoadConfigurationViewModel.cs
+++ DocumentPacker/DocumentPacker/ViewModels/SubViewModels/LoadConfigurationViewModel.cs
@@ -1,5 +1,6 @@
 ﻿namespace DocumentPacker.ViewModels.SubViewModels;
 
+using System.Windows.Input;
 using DocumentPacker.Contracts;
 using DocumentPacker.Contracts.ViewModels.SubViewModels;
 
@@ -10,4 +11,28 @@ using DocumentPacker.Contracts.ViewModels.SubViewModels;
 /// <seealso cref="DocumentPacker.Contracts.ViewModels.SubViewModels.ILoadConfigurationViewModel" />
 internal class LoadConfigurationViewModel() : SubViewModel(SubViewId.LoadConfiguration), ILoadConfigurationViewModel
 {
+    /// <summary>
+    ///     The configuration file name.
+    /// </summary>
+    private string configurationFileName = string.Empty;
+
+    /// <summary>
+    ///     Gets or sets the name of the configuration file.
+    /// </summary>
+    public string ConfigurationFileName
+    {
+        get => this.configurationFileName;
+        set =>
+            this.SetField(
+                ref this.configurationFileName,
+                value);
+    }
+
+    /// <summary>
+    ///     Gets the load configuration file command.
+    /// </summary>
+    public ICommand LoadConfigurationFileCommand =>
+        new TaskCommand(
+            fileName => fileName is not null && !string.IsNullOrWhiteSpace(fileName as string),
+            (fileName, cancellationToken) => Task.CompletedTask);
 }
