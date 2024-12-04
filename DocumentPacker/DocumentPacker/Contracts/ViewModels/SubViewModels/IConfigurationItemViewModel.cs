163104aa7630a1b5f7e325eeb70d3eba16b09744
MichaelDiers <md@sreid.de>
2024-12-04 23:44:37 +0100

    add configuration item

---
+++ DocumentPacker/DocumentPacker/Contracts/ViewModels/SubViewModels/IConfigurationItemViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace DocumentPacker.Contracts.ViewModels.SubViewModels;
+
+/// <summary>
+///     Describes a configuration item.
+/// </summary>
+public interface IConfigurationItemViewModel
+{
+    /// <summary>
+    ///     Gets or sets the type of the configuration item.
+    /// </summary>
+    ConfigurationItemType ConfigurationItemType { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the configuration value.
+    /// </summary>
+    string? Value { get; set; }
+}
---
+++ DocumentPacker/DocumentPacker/ViewModels/SubViewModels/ConfigurationItemViewModel.cs
@@ -0,0 +1,44 @@
+﻿namespace DocumentPacker.ViewModels.SubViewModels;
+
+using DocumentPacker.Contracts;
+
+/// <summary>
+///     Describes a configuration item.
+/// </summary>
+/// <seealso cref="DocumentPacker.ViewModels.BaseViewModel" />
+internal class ConfigurationItemViewModel : BaseViewModel
+{
+    /// <summary>
+    ///     The configuration item type.
+    /// </summary>
+    private ConfigurationItemType configurationItemType;
+
+    /// <summary>
+    ///     The value of the item.
+    /// </summary>
+    private string? value;
+
+    /// <summary>
+    ///     Gets or sets the type of the configuration item.
+    /// </summary>
+    public ConfigurationItemType ConfigurationItemType
+    {
+        get => this.configurationItemType;
+        set =>
+            this.SetField(
+                ref this.configurationItemType,
+                value);
+    }
+
+    /// <summary>
+    ///     Gets or sets the configuration value.
+    /// </summary>
+    public string? Value
+    {
+        get => this.value;
+        set =>
+            this.SetField(
+                ref this.value,
+                value);
+    }
+}
