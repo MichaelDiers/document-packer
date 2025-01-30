2b6bfe4ff231feada3fb0b9d2ff746d3dd951595
MichaelDiers <md@sreid.de>
2025-01-30 01:38:28 +0100

    add back link handling

---
+++ DocumentPacker/DocumentPacker/Parts2/Header/Links/BackLinkPart/BackLinkView.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace DocumentPacker.Parts2.Header.Links.BackLinkPart;
+
+using DocumentPacker.EventHandling;
+
+/// <summary>
+///     Interaction logic for BackLinkView.xaml
+/// </summary>
+[DataContext(ApplicationElementPart.BackLink)]
+public partial class BackLinkView : IApplicationView
+{
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="BackLinkView" /> class.
+    /// </summary>
+    public BackLinkView()
+    {
+        this.InitializeComponent();
+    }
+}
