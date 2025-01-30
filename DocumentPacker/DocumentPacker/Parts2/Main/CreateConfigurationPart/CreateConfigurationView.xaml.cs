14309e42cf6228d7b07fa7bde780676597caa1d9
MichaelDiers <md@sreid.de>
2025-01-30 03:43:19 +0100

    add the create-configuration part

---
+++ DocumentPacker/DocumentPacker/Parts2/Main/CreateConfigurationPart/CreateConfigurationView.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace DocumentPacker.Parts2.Main.CreateConfigurationPart;
+
+using DocumentPacker.EventHandling;
+
+/// <summary>
+///     Interaction logic for CreateConfigurationView.xaml
+/// </summary>
+[DataContext(ApplicationElementPart.Main)]
+public partial class CreateConfigurationView : IApplicationView
+{
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="CreateConfigurationView" /> class.
+    /// </summary>
+    public CreateConfigurationView()
+    {
+        this.InitializeComponent();
+    }
+}
---
+++ DocumentPacker/DocumentPacker/Parts2/Main/CreateConfigurationPart/CreateConfigurationViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DocumentPacker.Parts2.Main.CreateConfigurationPart;
+
+using DocumentPacker.Mvvm;
+
+/// <summary>
+///     The view model of <see cref="CreateConfigurationView" />.
+/// </summary>
+/// <seealso cref="DocumentPacker.Mvvm.ApplicationViewModel" />
+internal class CreateConfigurationViewModel : ApplicationViewModel
+{
+}
