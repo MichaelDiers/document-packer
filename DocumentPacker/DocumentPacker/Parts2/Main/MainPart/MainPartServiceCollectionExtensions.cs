a6832c92631e9e476a604e6d427cb0cd9d09cecd
MichaelDiers <md@sreid.de>
2025-01-30 02:12:20 +0100

    add the main part

---
+++ DocumentPacker/DocumentPacker/Parts2/Main/MainPart/MainPartServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace DocumentPacker.Parts2.Main.MainPart;
+
+using DocumentPacker.EventHandling;
+using Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+///     Extensions for <see cref="IServiceCollection" />
+/// </summary>
+public static class MainPartServiceCollectionExtensions
+{
+    /// <summary>
+    ///     Adds the main part of the application.
+    /// </summary>
+    /// <param name="services">Dependencies are added to this <see cref="IServiceCollection" />.</param>
+    /// <returns>The given <paramref name="services" />.</returns>
+    public static IServiceCollection AddMainPart(this IServiceCollection services)
+    {
+        services.AddKeyedTransient<IApplicationView, MainView>(ApplicationElementPart.Main);
+        services.AddKeyedTransient<IApplicationViewModel, MainViewModel>(ApplicationElementPart.Main);
+
+        return services;
+    }
+}
---
+++ DocumentPacker/DocumentPacker/Parts2/Main/MainPart/MainView.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace DocumentPacker.Parts2.Main.MainPart;
+
+using DocumentPacker.EventHandling;
+
+/// <summary>
+///     Interaction logic for MainView.xaml
+/// </summary>
+[DataContext(ApplicationElementPart.Main)]
+public partial class MainView : IApplicationView
+{
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="MainView" /> class.
+    /// </summary>
+    public MainView()
+    {
+        this.InitializeComponent();
+    }
+}
