5c96a2afc5d4d002fd2c63e06bde1343f48d9678
MichaelDiers <md@sreid.de>
2025-01-30 01:54:06 +0100

    add the footer part

---
+++ DocumentPacker/DocumentPacker/Parts2/FooterPart/FooterView.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace DocumentPacker.Parts2.FooterPart;
+
+using DocumentPacker.EventHandling;
+using DocumentPacker.Mvvm;
+
+/// <summary>
+///     Interaction logic for FooterView.xaml
+/// </summary>
+[DataContext(ApplicationElementPart.Footer)]
+public partial class FooterView : BaseUserControl, IApplicationView
+{
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="FooterView" /> class.
+    /// </summary>
+    public FooterView()
+    {
+        this.InitializeComponent();
+    }
+}
---
+++ DocumentPacker/DocumentPacker/Parts2/FooterPart/FooterViewModel.cs
@@ -0,0 +1,31 @@
+﻿namespace DocumentPacker.Parts2.FooterPart;
+
+using DocumentPacker.Mvvm;
+using DocumentPacker.Resources;
+
+/// <summary>
+///     View model of the <see cref="FooterView" />.
+/// </summary>
+/// <seealso cref="DocumentPacker.Mvvm.ApplicationViewModel" />
+internal class FooterViewModel : ApplicationViewModel
+{
+    /// <summary>
+    ///     The version information of the application.
+    /// </summary>
+    private string version = string.Format(
+        Translation.FooterPartVersion,
+        Translation.DocumentPacker,
+        "0.0.1");
+
+    /// <summary>
+    ///     Gets or sets the version information.
+    /// </summary>
+    public string Version
+    {
+        get => this.version;
+        set =>
+            this.SetField(
+                ref this.version,
+                value);
+    }
+}
