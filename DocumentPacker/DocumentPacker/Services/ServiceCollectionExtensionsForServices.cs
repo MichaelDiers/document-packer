b12ccefe9f29a0e79e4c02e3814376e90bb75278
MichaelDiers <md@sreid.de>
2025-02-02 00:08:52 +0100

    add service di

---
+++ DocumentPacker/DocumentPacker/Services/ServiceCollectionExtensionsForServices.cs
@@ -0,0 +1,46 @@
+﻿namespace DocumentPacker.Services;
+
+using DocumentPacker.Services.Crypto;
+using DocumentPacker.Services.Zip;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+/// <summary>
+///     Add services dependencies by using extensions for <see cref="IServiceCollection" />.
+/// </summary>
+public static class ServiceCollectionExtensionsForServices
+{
+    /// <summary>
+    ///     Tries to add all services as a dependency.
+    /// </summary>
+    /// <param name="services">Dependencies are added to this <see cref="IServiceCollection" />.</param>
+    /// <returns>The given <paramref name="services" />.</returns>
+    public static IServiceCollection TryAddAllServices(this IServiceCollection services)
+    {
+        return services.TryAddZipServices().TryAddCryptoServices();
+    }
+
+    /// <summary>
+    ///     Tries to add all services that handle crypto data as a dependency.
+    /// </summary>
+    /// <param name="services">Dependencies are added to this <see cref="IServiceCollection" />.</param>
+    /// <returns>The given <paramref name="services" />.</returns>
+    public static IServiceCollection TryAddCryptoServices(this IServiceCollection services)
+    {
+        services.TryAddSingleton<ICryptoFactory, CryptoFactory>();
+
+        return services;
+    }
+
+    /// <summary>
+    ///     Tries to add all services that handle zip files as a dependency.
+    /// </summary>
+    /// <param name="services">Dependencies are added to this <see cref="IServiceCollection" />.</param>
+    /// <returns>The given <paramref name="services" />.</returns>
+    public static IServiceCollection TryAddZipServices(this IServiceCollection services)
+    {
+        services.TryAddSingleton<IZipFileCreator, ZipFileCreator>();
+
+        return services;
+    }
+}
