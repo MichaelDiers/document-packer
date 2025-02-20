86ac519865c79abc6e8d115298f468b67f48de64
MichaelDiers <md@sreid.de>
2025-02-20 23:37:51 +0100

    update footer

--- DocumentPacker/DocumentPacker/Parts/FooterPart/FooterView.xaml.cs
+++ DocumentPacker/DocumentPacker/Parts/FooterPart/FooterView.xaml.cs
@@ -1,13 +1,12 @@
 ﻿namespace DocumentPacker.Parts.FooterPart;
 
 using DocumentPacker.EventHandling;
-using DocumentPacker.Mvvm;
 
 /// <summary>
 ///     Interaction logic for FooterView.xaml
 /// </summary>
 [DataContext(ApplicationElementPart.Footer)]
-public partial class FooterView : BaseUserControl, IApplicationView
+public partial class FooterView : IApplicationView
 {
     /// <summary>
     ///     Initializes a new instance of the <see cref="FooterView" /> class.
