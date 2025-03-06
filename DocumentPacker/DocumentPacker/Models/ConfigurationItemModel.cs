e904d82e46c25a92dd71717c347133641dd9cf03
MichaelDiers <md@sreid.de>
2025-03-06 23:25:11 +0100

    add configuration models

---
+++ DocumentPacker/DocumentPacker/Models/ConfigurationItemModel.cs
@@ -0,0 +1,36 @@
+﻿namespace DocumentPacker.Models;
+
+using DocumentPacker.Parts.Main.CreateConfigurationPart.ViewModels;
+
+/// <summary>
+///     Describes the data of a document packer configuration item.
+/// </summary>
+public class ConfigurationItemModel
+{
+    /// <summary>
+    ///     Describes the data of a document packer configuration item.
+    /// </summary>
+    public ConfigurationItemModel(ConfigurationItemType configurationItemType, bool isRequired, string itemDescription)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(itemDescription);
+
+        this.ConfigurationItemType = configurationItemType;
+        this.IsRequired = isRequired;
+        this.ItemDescription = itemDescription;
+    }
+
+    /// <summary>
+    ///     Gets the type of the configuration item.
+    /// </summary>
+    public ConfigurationItemType ConfigurationItemType { get; }
+
+    /// <summary>
+    ///     Gets a value that specifies if the value is required.
+    /// </summary>
+    public bool IsRequired { get; }
+
+    /// <summary>
+    ///     Gets the item description.
+    /// </summary>
+    public string ItemDescription { get; }
+}
---
+++ DocumentPacker/DocumentPacker/Models/ConfigurationModel.cs
@@ -0,0 +1,43 @@
+﻿namespace DocumentPacker.Models;
+
+/// <summary>
+///     Describes the data of a document packer configuration.
+/// </summary>
+public class ConfigurationModel
+{
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="ConfigurationModel" /> class.
+    /// </summary>
+    public ConfigurationModel(
+        IEnumerable<ConfigurationItemModel> configurationItems,
+        string description,
+        string rsaPrivateKey,
+        string rsaPublicKey
+    )
+    {
+        this.ConfigurationItems = configurationItems.ToArray();
+        this.Description = description;
+        this.RsaPrivateKey = rsaPrivateKey;
+        this.RsaPublicKey = rsaPublicKey;
+    }
+
+    /// <summary>
+    ///     Gets the configuration items.
+    /// </summary>
+    public IEnumerable<ConfigurationItemModel> ConfigurationItems { get; }
+
+    /// <summary>
+    ///     Gets the description of the configuration.
+    /// </summary>
+    public string Description { get; }
+
+    /// <summary>
+    ///     Gets the private rsa key in pem format.
+    /// </summary>
+    public string? RsaPrivateKey { get; set; }
+
+    /// <summary>
+    ///     Gets the public rsa key in pem format.
+    /// </summary>
+    public string RsaPublicKey { get; }
+}
