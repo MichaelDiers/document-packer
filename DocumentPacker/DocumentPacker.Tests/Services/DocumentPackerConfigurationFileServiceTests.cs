9bd583160f1b57a11f08a0ee90614b46ec3f0307
MichaelDiers <md@sreid.de>
2025-03-06 23:28:16 +0100

    add tests

---
+++ DocumentPacker/DocumentPacker.Tests/Services/DocumentPackerConfigurationFileServiceTests.cs
@@ -0,0 +1,115 @@
+﻿namespace DocumentPacker.Tests.Services;
+
+using DocumentPacker.Models;
+using DocumentPacker.Parts.Main.CreateConfigurationPart.ViewModels;
+using DocumentPacker.Services;
+using Libs.Wpf.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+public class DocumentPackerConfigurationFileServiceTests : IDisposable
+{
+    private readonly ConfigurationModel configurationModel = new(
+        [
+            new ConfigurationItemModel(
+                ConfigurationItemType.File,
+                true,
+                Guid.NewGuid().ToString()),
+            new ConfigurationItemModel(
+                ConfigurationItemType.Text,
+                false,
+                Guid.NewGuid().ToString())
+        ],
+        Guid.NewGuid().ToString(),
+        Guid.NewGuid().ToString(),
+        Guid.NewGuid().ToString());
+
+    private readonly string password = Guid.NewGuid().ToString();
+
+    private readonly FileInfo privateConfigurationFile = new(Guid.NewGuid().ToString());
+
+    private readonly FileInfo publicConfigurationFile = new(Guid.NewGuid().ToString());
+
+    private readonly IDocumentPackerConfigurationFileService service = CustomServiceProviderBuilder
+        .Build(ServiceCollectionExtensionsForServices.TryAddAllServices)
+        .GetRequiredService<IDocumentPackerConfigurationFileService>();
+
+    /// <summary>Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.</summary>
+    public void Dispose()
+    {
+        if (this.privateConfigurationFile.Exists)
+        {
+            this.privateConfigurationFile.Delete();
+        }
+
+        if (this.publicConfigurationFile.Exists)
+        {
+            this.publicConfigurationFile.Delete();
+        }
+    }
+
+    [Fact]
+    public async Task FromFileAsync()
+    {
+        await this.service.ToFileAsync(
+            this.privateConfigurationFile,
+            this.publicConfigurationFile,
+            this.password,
+            this.configurationModel,
+            TestContext.Current.CancellationToken);
+
+        Assert.True(this.privateConfigurationFile.Exists);
+        Assert.True(this.publicConfigurationFile.Exists);
+
+        var privateConfiguration = await this.service.FromFileAsync(
+            this.privateConfigurationFile,
+            this.password,
+            TestContext.Current.CancellationToken);
+        this.AssertConfiguration(privateConfiguration);
+
+        var publicConfiguration = await this.service.FromFileAsync(
+            this.publicConfigurationFile,
+            this.password,
+            TestContext.Current.CancellationToken);
+        Assert.Null(publicConfiguration.RsaPrivateKey);
+        publicConfiguration.RsaPrivateKey = privateConfiguration.RsaPrivateKey;
+        this.AssertConfiguration(publicConfiguration);
+    }
+
+    [Fact]
+    public async Task ToFileAsync()
+    {
+        await this.service.ToFileAsync(
+            this.privateConfigurationFile,
+            this.publicConfigurationFile,
+            this.password,
+            this.configurationModel,
+            TestContext.Current.CancellationToken);
+
+        Assert.True(this.privateConfigurationFile.Exists);
+        Assert.True(this.publicConfigurationFile.Exists);
+    }
+
+    private void AssertConfiguration(ConfigurationModel actual)
+    {
+        Assert.Equal(
+            this.configurationModel.Description,
+            actual.Description);
+        Assert.Equal(
+            this.configurationModel.RsaPrivateKey,
+            actual.RsaPrivateKey);
+        Assert.Equal(
+            this.configurationModel.RsaPublicKey,
+            actual.RsaPublicKey);
+        Assert.Equal(
+            this.configurationModel.ConfigurationItems.Count(),
+            actual.ConfigurationItems.Count());
+        foreach (var expectedItem in this.configurationModel.ConfigurationItems)
+        {
+            Assert.Contains(
+                actual.ConfigurationItems,
+                item => expectedItem.IsRequired == item.IsRequired &&
+                        expectedItem.ItemDescription == item.ItemDescription &&
+                        expectedItem.ConfigurationItemType == item.ConfigurationItemType);
+        }
+    }
+}
---
+++ DocumentPacker/DocumentPacker.Tests/Services/RsaServiceTests.cs
@@ -0,0 +1,61 @@
+﻿namespace DocumentPacker.Tests.Services;
+
+using DocumentPacker.Services;
+using Libs.Wpf.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+public class RsaServiceTests
+{
+    private readonly IRsaService service = CustomServiceProviderBuilder
+        .Build(ServiceCollectionExtensionsForServices.TryAddAllServices)
+        .GetRequiredService<IRsaService>();
+
+    [Fact]
+    public void GenerateKeys()
+    {
+        var (privateKey, publicKey) = this.service.GenerateKeys();
+
+        Assert.False(string.IsNullOrWhiteSpace(privateKey));
+        Assert.False(string.IsNullOrWhiteSpace(publicKey));
+    }
+
+    [Fact]
+    public void ValidateKeys()
+    {
+        var (privateKey, publicKey) = this.service.GenerateKeys();
+
+        Assert.False(string.IsNullOrWhiteSpace(privateKey));
+        Assert.False(string.IsNullOrWhiteSpace(publicKey));
+
+        Assert.True(
+            this.service.ValidateKeys(
+                privateKey,
+                publicKey));
+    }
+
+    [Fact]
+    public void ValidateKeys_ShouldFail_IfKeysAreInvalid()
+    {
+        Assert.False(
+            this.service.ValidateKeys(
+                Guid.NewGuid().ToString(),
+                Guid.NewGuid().ToString()));
+    }
+
+    [Fact]
+    public void ValidateKeys_ShouldFail_IfPrivateAndPublicKeyDoNotMatch()
+    {
+        var (privateKey1, publicKey1) = this.service.GenerateKeys();
+        var (privateKey2, publicKey2) = this.service.GenerateKeys();
+
+        Assert.False(
+            this.service.ValidateKeys(
+                privateKey1,
+                publicKey2));
+
+        Assert.False(
+            this.service.ValidateKeys(
+                privateKey2,
+                publicKey1));
+    }
+}
