6e98ca6333ad339f51677b432d86b86cd4669180
MichaelDiers <md@sreid.de>
2025-03-10 02:16:26 +0100

    add default commands

---
+++ DocumentPacker/DocumentPacker/Commands/SelectFileCommand.cs
@@ -0,0 +1,22 @@
+﻿namespace DocumentPacker.Commands;
+
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using Libs.Wpf.Commands;
+using Libs.Wpf.ViewModels;
+
+internal class
+    SelectFileCommand<TCommandParameter>(
+        ICommandFactory commandFactory,
+        Action<TCommandParameter?, string> execute,
+        string? filter = null
+    ) : TranslatableButton<ICommand>(
+    commandFactory.CreateOpenFileDialogCommand(
+        execute,
+        filter),
+    new BitmapImage(
+        new Uri(
+            "pack://application:,,,/DocumentPacker;component/Assets/material_symbol_file_open.png",
+            UriKind.Absolute)),
+    CommandTranslations.ResourceManager,
+    toolTipResourceKey: nameof(CommandTranslations.SelectFileCommandToolTip));
---
+++ DocumentPacker/DocumentPacker/Commands/SelectFolderCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace DocumentPacker.Commands;
+
+using System.IO;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using Libs.Wpf.Commands;
+using Libs.Wpf.ViewModels;
+
+internal class SelectFolderCommand(ICommandFactory commandFactory, Action<string> execute)
+    : TranslatableButton<ICommand>(
+        commandFactory.CreateOpenFolderDialogCommand(
+            new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)),
+            execute),
+        new BitmapImage(
+            new Uri(
+                "pack://application:,,,/DocumentPacker;component/Assets/material_symbol_folder.png",
+                UriKind.Absolute)),
+        CommandTranslations.ResourceManager,
+        toolTipResourceKey: nameof(CommandTranslations.SelectFolderCommandToolTip));
