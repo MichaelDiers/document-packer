e5399a1cccce5758a85a34011535c5d130baad11
MichaelDiers <md@sreid.de>
2025-03-20 09:34:12 +0100

    add comments

--- DocumentPacker/DocumentPacker/Commands/SelectFileCommand.cs
+++ DocumentPacker/DocumentPacker/Commands/SelectFileCommand.cs
@@ -4,7 +4,15 @@ using System.Windows.Input;
 using DocumentPacker.Extensions;
 using Libs.Wpf.Commands;
 using Libs.Wpf.Localization;
+using Microsoft.Win32;
 
+/// <summary>
+///     The data of a <see cref="TranslatableButton{TCommand}" /> that opens an <see cref="OpenFileDialog" />.
+/// </summary>
+/// <typeparam name="TCommandParameter">The type of the command parameter.</typeparam>
+/// <param name="commandFactory">A factory for creating commands.</param>
+/// <param name="execute">The execute method of the command.</param>
+/// <param name="filter">An optional filter of the <see cref="OpenFileDialog" /> for selectable files.</param>
 internal class SelectFileCommand<TCommandParameter>(
     ICommandFactory commandFactory,
     Action<TCommandParameter?, string> execute,
--- DocumentPacker/DocumentPacker/Commands/SelectFolderCommand.cs
+++ DocumentPacker/DocumentPacker/Commands/SelectFolderCommand.cs
@@ -5,7 +5,13 @@ using System.Windows.Input;
 using DocumentPacker.Extensions;
 using Libs.Wpf.Commands;
 using Libs.Wpf.Localization;
+using Microsoft.Win32;
 
+/// <summary>
+///     The data of a <see cref="TranslatableButton{TCommand}" /> that opens an <see cref="OpenFolderDialog" />.
+/// </summary>
+/// <param name="commandFactory">A factory for creating commands.</param>
+/// <param name="execute">The execute method of the command.</param>
 internal class SelectFolderCommand(ICommandFactory commandFactory, Action<string> execute)
     : TranslatableButton<ICommand>(
         commandFactory.CreateOpenFolderDialogCommand(
