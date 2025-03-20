761c42c31d7df695ca1ca39784c0f16a8e037436
MichaelDiers <md@sreid.de>
2025-03-20 01:47:39 +0100

    add tests

--- DocumentPacker/DocumentPacker/Commands/CommandExecutor.cs
+++ DocumentPacker/DocumentPacker/Commands/CommandExecutor.cs
@@ -3,8 +3,19 @@
 using Libs.Wpf.Controls.CustomMessageBox;
 using Libs.Wpf.Localization;
 
-internal static class CommandExecutor
+/// <summary>
+///     A helper to execute commands.
+/// </summary>
+public static class CommandExecutor
 {
+    /// <summary>
+    ///     Execute <paramref name="execute" /> and handles errors.
+    /// </summary>
+    /// <param name="validate">A validation function that is called before <paramref name="execute" />.</param>
+    /// <param name="commandSync">The command sync that ensures that only one command is executed at the same time.</param>
+    /// <param name="execute">The execute method of the command.</param>
+    /// <param name="translatableCancellableButton">The cancel button to abort the execution.</param>
+    /// <returns>A <see cref="Task{T}" /> whose indicates success and a message.</returns>
     public static async Task<(bool success, string? message)> Execute(
         Func<bool> validate,
         ICommandSync commandSync,
@@ -44,6 +55,11 @@ internal static class CommandExecutor
         }
     }
 
+    /// <summary>
+    ///     Executes the post execute method of a command and handles errors.
+    /// </summary>
+    /// <param name="task">The result <see cref="Task{T}" /> of the command execution.</param>
+    /// <param name="messageBoxService">A service for showing a message box.</param>
     public static void PostExecute(Task<(bool success, string? message)> task, IMessageBoxService messageBoxService)
     {
         try
