1b5c3374faa6495b7a9a0624555e4745fcba0183
MichaelDiers <md@sreid.de>
2025-03-31 18:00:51 +0200

    fix code issues

--- DocumentPacker/DocumentPacker/Parts/Main/EncryptPart/ViewModels/IEncryptDataItemViewModel.cs
+++ DocumentPacker/DocumentPacker/Parts/Main/EncryptPart/ViewModels/IEncryptDataItemViewModel.cs
@@ -4,14 +4,44 @@ using System.Windows.Input;
 using DocumentPacker.Parts.Main.CreateConfigurationPart.ViewModels;
 using Libs.Wpf.Localization;
 
+/// <summary>
+///     Describes a data item that gets encrypted.
+/// </summary>
 public interface IEncryptDataItemViewModel
 {
+    /// <summary>
+    ///     Gets or sets the configuration item type.
+    /// </summary>
     ConfigurationItemType ConfigurationItemType { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the description.
+    /// </summary>
     TranslatableAndValidable<string> Description { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the id.
+    /// </summary>
     TranslatableAndValidable<string> Id { get; }
+
+    /// <summary>
+    ///     Gets or sets the value that specifies if the value is required.
+    /// </summary>
     TranslatableAndValidable<bool> IsRequired { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the select file command.
+    /// </summary>
     TranslatableButton<ICommand> SelectFileCommand { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the value.
+    /// </summary>
     TranslatableAndValidable<string> Value { get; set; }
 
+    /// <summary>
+    ///     Validates the view model.
+    /// </summary>
+    /// <returns><c>True</c> if the validation succeeds; <c>false</c> otherwise.</returns>
     bool Validate();
 }
--- DocumentPacker/DocumentPacker/Parts/Main/EncryptPart/ViewModels/IEncryptDataViewModel.cs
+++ DocumentPacker/DocumentPacker/Parts/Main/EncryptPart/ViewModels/IEncryptDataViewModel.cs
@@ -1,14 +1,31 @@
-﻿using Libs.Wpf.Localization;
+﻿namespace DocumentPacker.Parts.Main.EncryptPart.ViewModels;
+
 using System.Collections.ObjectModel;
+using Libs.Wpf.Localization;
 
-namespace DocumentPacker.Parts.Main.EncryptPart.ViewModels
+/// <summary>
+///     Describes the data that gets encrypted.
+/// </summary>
+public interface IEncryptDataViewModel
 {
-    public interface IEncryptDataViewModel
-    {
-        TranslatableAndValidable<string>? Description { get; set; }
-        ObservableCollection<IEncryptDataItemViewModel>? Items { get; set; }
-        string RsaPublicKey { get; set; }
+    /// <summary>
+    ///     Gets or sets the description.
+    /// </summary>
+    TranslatableAndValidable<string>? Description { get; set; }
 
-        bool Validate();
-    }
+    /// <summary>
+    ///     Gets or sets the items.
+    /// </summary>
+    ObservableCollection<IEncryptDataItemViewModel>? Items { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the rsa public key.
+    /// </summary>
+    string RsaPublicKey { get; set; }
+
+    /// <summary>
+    ///     Validates the view model.
+    /// </summary>
+    /// <returns><c>True</c> if the validation succeeds; <c>false</c> otherwise.</returns>
+    bool Validate();
 }
